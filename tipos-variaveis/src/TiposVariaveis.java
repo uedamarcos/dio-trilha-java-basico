a85d3ff2f5f0fa447ccc40a676420b75e8407ff8
uedamarcos <ueda.marcos@yahoo.com.br>
2024-09-18 23:29:00 -0400

    alterações diversas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+        System.out.println(numero);
+        final double VALOR_DE_PI = 3.14;
+        
     }
 }
