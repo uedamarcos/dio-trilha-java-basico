f511a33e612d2321c8053bc1e27d65950c4d6642
uedamarcos <ueda.marcos@yahoo.com.br>
2024-09-22 14:20:55 -0400

    commit de novo

---
+++ Calculadora/Calculadora/src/Calculadora.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre os números inteiros
+ * <p>
+ * <b>Note: </b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Marcos Ueda
+ * @version 1.0
+ * @since 22/09/2024
+ */
+
+ public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é o resultado da soma dos dois números
+     */
+
+     public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+     }
+}
---
+++ Calculadora/Calculadora/src/Comentarios.java
@@ -0,0 +1,27 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+
+  
+    }
+          /** 
+         * Estas duas estrelinhas acima 
+         * é para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!*/
+        public void metodo (){
+
+        }
+        // no terminal do VS code, digitar o comando abaixo:
+        // javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
+        // executando o comando acima, foi criada a pasta abaixo:
+        // C:\dev\projetos\estudos\dio-trilha-java-basico\Calculadora\docs
+        // neste diretório tem index.html
+}
