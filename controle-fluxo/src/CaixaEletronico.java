ce476223a5a7b6686056417f45f8888479bdda6e
uedamarcos <ueda.marcos@yahoo.com.br>
2024-09-22 22:40:28 -0400

    commit de novo

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -2,11 +2,14 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado=26.0;
+        double valorSolicitado=15.0;
 
-        if(valorSolicitado<saldo)
+        if(valorSolicitado<saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }
+        else
+            System.out.println("Saldo insuficiente");
 
-        System.out.println(saldo);
     }
 }
