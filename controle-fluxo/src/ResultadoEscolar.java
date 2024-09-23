ef112ed32d24ea37293a8d152332197dc0173309
uedamarcos <ueda.marcos@yahoo.com.br>
2024-09-22 23:04:52 -0400

    Commit 23:04

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -4,6 +4,9 @@ public class ResultadoEscolar {
         int nota=6;
         if(nota>=7)
             System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota <7)
+            System.out.println("Prova de Recuperação");
         else
             System.out.println("Reprovado");
     }
---
+++ controle-fluxo/src/ResultadoEscolar2.java
@@ -0,0 +1,9 @@
+//ResultadoEscolar2.java
+public class ResultadoEscolar2 {
+    public static void main(String[] args) {
+        int nota=7;
+        String resultado = nota >= 7 ? "Aprovado": nota >5 && nota <7 ? "Prova de Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
+    }
+}
