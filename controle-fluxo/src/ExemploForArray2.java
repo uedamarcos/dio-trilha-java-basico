aa79dec2ffbe91cdadac3f5a325889d5e64e5dee
uedamarcos <ueda.marcos@yahoo.com.br>
2024-09-23 22:19:39 -0400

    Commit 23/09/2024 22:19

---
+++ controle-fluxo/src/ExemploForArray2.java
@@ -0,0 +1,11 @@
+public class ExemploForArray2 {
+    public static void main(String[] args) {
+
+        String alunos[]={"FELIPE","JONAS","JULIA","MARCOS"};
+        // em arrays o índice dos elementos inicia com valor 0 (zero)
+        for (String aluno : alunos){
+
+            System.out.println("Nome do aluno é: " + aluno) ;
+        }
+    }
+}
